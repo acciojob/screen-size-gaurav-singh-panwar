b8ae8310739fbd8cf27c39cd9833537736c7b4ee
gaurav-singh-panwar <55618776+gaurav-singh-panwar@users.noreply.github.com>
2023-07-18 19:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let div=document.createElement('div');
+div.id="sizeInfo";
+let h1=document.createElement('h1');
+h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+// console.log(h1.innerText);
+document.body.append(div);
+div.append(h1);
+
+
+window.onresize=function(){
+    h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+};
